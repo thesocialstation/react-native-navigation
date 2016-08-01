adba80fa0191275052d821945772f229e8775f96
Ron Heft <ron@thesocialstation.com>
2016-08-01 14:42:54 -0400

    Fix device rotation

--- android/app/src/main/java/com/reactnativenavigation/activities/SingleScreenActivity.java
+++ android/app/src/main/java/com/reactnativenavigation/activities/SingleScreenActivity.java
@@ -55,14 +55,12 @@ public class SingleScreenActivity extends BaseReactActivity {
 
     @Override
     public void push(Screen screen) {
-        super.push(screen);
         mScreenStack.push(screen);
         StyleHelper.updateStyles(mToolbar, screen);
     }
 
     @Override
     public Screen pop(String navigatorId) {
-        super.pop(navigatorId);
         Screen popped = mScreenStack.pop();
         StyleHelper.updateStyles(mToolbar, getCurrentScreen());
         return popped;
@@ -70,7 +68,6 @@ public class SingleScreenActivity extends BaseReactActivity {
 
     @Override
     public Screen popToRoot(String navigatorId) {
-        super.popToRoot(navigatorId);
         Screen screen = mScreenStack.popToRoot();
         StyleHelper.updateStyles(mToolbar, getCurrentScreen());
         return screen;
@@ -78,7 +75,6 @@ public class SingleScreenActivity extends BaseReactActivity {
 
     @Override
     public Screen resetTo(Screen screen) {
-        super.resetTo(screen);
         Screen popped = mScreenStack.resetTo(screen);
         StyleHelper.updateStyles(mToolbar, screen);
         return popped;
